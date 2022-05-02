14692fe2dc57a074cb5ebfc913a91e967b990cd5
Ayush Rathore <94514710+ayushrathore111@users.noreply.github.com>
2022-05-02 14:12:54 +0530

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,13 @@
+import time
+import pyttsx3
+
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec,60)
+        timeformat = '{:02d}:{:02d}'.format(mins,secs)
+        print(timeformat,end='\r')
+        time.sleep(1)
+        time_sec -=1
+    print("Time Over.....")
+
+countdown(5)
